db4d69f2435bb04e5641ab1c0c2318fc88b0efc9
clementia1 <grininma0@gmail.com>
2021-07-19 23:03:49 +0300

    *

--- IdentityServer/Quickstart/TestUsers.cs
+++ IdentityServer/Quickstart/TestUsers.cs
@@ -19,7 +19,7 @@ namespace IdentityServer4.Quickstart.UI
                     new Claim(JwtClaimTypes.Name, "Alice Smith"),
                     new Claim(JwtClaimTypes.GivenName, "Alice"),
                     new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                    new Claim(JwtClaimTypes.Picture, "/assets/images/users/alice.jpg"),
+                    new Claim(JwtClaimTypes.Picture, "https://images.unsplash.com/photo-1476493279419-b785d41e38d8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=61eaea85f1aa3d065400179c78163f15"),
                     new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
                     new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
                     new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
@@ -29,9 +29,10 @@ namespace IdentityServer4.Quickstart.UI
             new TestUser{SubjectId = "88421113", Username = "bob", Password = "bob", 
                 Claims = 
                 {
-                    new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                    new Claim(JwtClaimTypes.Name, "Bob Martin"),
                     new Claim(JwtClaimTypes.GivenName, "Bob"),
-                    new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    new Claim(JwtClaimTypes.FamilyName, "Martin"),
+                    new Claim(JwtClaimTypes.Picture, "https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=f05c14dd4db49f08a789e6449604c490"),
                     new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
                     new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
                     new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
