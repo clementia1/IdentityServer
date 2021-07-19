e107135f15b80a3183955a9e50fea83bf2d8ab5a
clementia1 <grininma0@gmail.com>
2021-07-19 08:29:42 +0300

    Initial commit

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,73 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("website.com")
+                {
+                    Scopes = new List<Scope>
+                    {
+                        new Scope("website.com")
+                    },
+                },
+                new ApiResource("webapi")
+                {
+                    Scopes = new List<Scope>
+                    {
+                        new Scope("webapi.webapi")
+                    },
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "pkce_client",
+                    ClientName = "React PKCE Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RedirectUris = { "http://localhost:9000" },
+                    AllowedScopes = { "openid", "profile", "website.com" },
+                    RequirePkce = true,
+                    RequireConsent = false,
+                    AllowPlainTextPkce = false
+                },
+                new Client
+                {
+                    ClientId = "webapi_client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "webapi.webapi" }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4.Quickstart.UI;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            
+            services.ConfigureNonBreakingSameSiteCookies();
+
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(TestUsers.Users)
+                .AddDeveloperSigningCredential();
+            
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            
+            app.UseCookiePolicy();
+            
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+        }
+    }
+}
